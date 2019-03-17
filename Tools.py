7e6a7e6fe14af0eb31f5d54a7b6f73042da7fdd8
Eshel Sinclair <eshel.sinclair@gmail.com>
2019-03-17 11:49:53 +0200

    added file for tools

---
+++ Tools.py
@@ -0,0 +1,15 @@
+import sys, os, subprocess
+
+
+def open_file(filename):
+    """
+    Open a file with the os's default application on any platform.
+    os.startfile is only supported on Windows...
+    :param filename: String; file to open
+    :return:
+    """
+    if sys.platform == "win32":
+        os.startfile(filename)
+    else:
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, filename])
