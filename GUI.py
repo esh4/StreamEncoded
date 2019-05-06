85fb246967b951f8c3e158de06704c7f14fcb27f
YonaGilZeller <yonagilzeller@gmail.com>
2019-05-06 16:38:24 +0300

    update 2
    update 2

--- GUI.py
+++ GUI.py
@@ -6,6 +6,7 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 
 
 class Management(ScreenManager):
@@ -23,17 +24,37 @@ class Encode(Screen, GridLayout):
         self.ids.img.source = self.ids.dir.text
 
 
+    def switch(self, current, next):
+        self.screens = ["width", "directory", "message", "location"]
+        self.index = self.screens.index(current)
+        if(self.index == 3):
+            self.index = 0
+        elif(self.index == -1):
+            self.index = 3
 
+        if(next == "up"):
+            self.index += 1
+        elif(next == "down"):
+            self.index -= 1
+
+        self.update()
+        return self.screens[self.index]
+
+    def firePopup(self):
+        pop = PopUps()
+        pop.open()
+
+    def encodePhoto(self):
+        pass
 
 
 class Decode(Screen, GridLayout):
     pass
 
-'''
-class SimplePopUp(Popup):
-    def displayImage(self, selection):
-        return selection
-'''
+
+class PopUps(Popup):
+    pass
+
 
 
 presentation = Builder.load_file("GUI.kv")
