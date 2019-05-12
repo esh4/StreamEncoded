29fcff6a282e88623be053be03104bf727caabb5
eshels <eshel.sinclair@gmail.com>
2019-05-12 11:16:36 +0300

    manual revert

--- GUI.py
+++ GUI.py
@@ -1,5 +1,3 @@
-import kivy
-kivy.require('1.9.1')
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -7,7 +5,16 @@ from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+from kivy.config import Config
 
+Config.set('graphics', 'width', '1000')
+Config.set('graphics', 'height', '750')
+
+
+from StartStopBitCodec import *
+from PIL import Image
+import kivy
+kivy.require('1.9.1')
 
 
 class Management(ScreenManager):
@@ -20,26 +27,21 @@ class HomeScreen(Screen,GridLayout):
 
 class Encode(Screen, GridLayout):
 
-
     def update(self):
         self.ids.img.source = self.ids.dir.text
-
 
     def switch(self, current, next):
         self.screens = ["width", "directory", "message", "location"]
         self.index = self.screens.index(current)
+        if(self.index == 3):
+            self.index = 0
+        elif(self.index == -1):
+            self.index = 3
 
         if(next == "up"):
-            if(self.index == 3):
-                self.index = 0
-            else:
-                self.index += 1
-
+            self.index += 1
         elif(next == "down"):
-            if(self.index == 0):
-                self.index = 3
-            else:
-                self.index -= 1
+            self.index -= 1
 
         self.update()
         return self.screens[self.index]
@@ -49,8 +51,20 @@ class Encode(Screen, GridLayout):
         pop.open()
 
     def encodePhoto(self):
-        pass
+        # defaults
+        self.ids.dir.text = r'testImg.png'
+        self.ids.mes.text = open('testMsg.txt', 'r').read()
+        self.ids.loc.text = '700 1500'
+        self.ids.wid.text = '200'
 
+        image_directory = self.ids.dir.text
+        image = Image.open(image_directory)
+        message = self.ids.mes.text
+        starting_location = (int(self.ids.loc.text.split()[0]), int(self.ids.loc.text.split()[1]))
+        message_width = int(self.ids.wid.text)
+        print(image_directory, message, starting_location, message_width)
+        encoded_image = encode_image(image, message, starting_location, message_width)
+        encoded_image.save(image_directory[:-4]+'__encoded.png')
 
 class Decode(Screen, GridLayout):
     pass
