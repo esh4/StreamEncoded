c9340c96e5f116fe0284b36c4032990bcc24b650
YonaGilZeller <yonagilzeller@gmail.com>
2019-05-14 08:45:55 +0300

    YGZ

--- GUI.py
+++ GUI.py
@@ -7,8 +7,8 @@ from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.config import Config
 
-# Config.set('graphics', 'width', '1000')
-# Config.set('graphics', 'height', '750')
+
+#sets the window to a static unchangable size
 Config.set('graphics', 'resizable', False)
 
 from StartStopBitCodec import *
@@ -20,37 +20,39 @@ import _thread
 import time
 kivy.require('1.9.1')
 
-
+#object that handles the screen switching
 class Management(ScreenManager):
     pass
 
-
+#home screen object
 class HomeScreen(Screen,GridLayout):
     pass
 
-
+#encode screen object
 class Encode(Screen, GridLayout):
+    #function to check whether the user has inputted a valid directory, so the image can be displayed
+    #in case the directory is invalid, image source defaults to the value of None, and displays nothing
     def update(self):
         self.ids.img.source = self.ids.dir.text
 
-    def switch(self, current, next):
+    # def switch(self, current, next):
+    #
+    #     self.screens = ["width", "directory", "message", "location"]
+    #     self.index = self.screens.index(current)
+    #     if(self.index == 3):
+    #         self.index = 0
+    #     elif(self.index == -1):
+    #         self.index = 3
+    #
+    #     if(next == "up"):
+    #         self.index += 1
+    #     elif(next == "down"):
+    #         self.index -= 1
+    #
+    #     self.update()
+    #     return self.screens[self.index]
 
-        self.screens = ["width", "directory", "message", "location"]
-        self.index = self.screens.index(current)
-        if(self.index == 3):
-            self.index = 0
-        elif(self.index == -1):
-            self.index = 3
-
-        if(next == "up"):
-            self.index += 1
-        elif(next == "down"):
-            self.index -= 1
-
-        self.update()
-        return self.screens[self.index]
-
-
+    #function that encodes the photo, based off of user inputted data in the GUI
     def encodePhoto(self):
         # defaults
         # self.ids.dir.text = r'testImg.png'
@@ -73,10 +75,10 @@ class Encode(Screen, GridLayout):
         send_image(encoded_image_dir, self.ids.ip.text)
 
 
-
+#decode screen object
 class Decode(Screen, GridLayout):
     decoded_msg = ''
-
+    #function that decodes the image
     def decode_image(self):
         # self.ids.dir2.text = 'testImg__encoded.png'
 
@@ -84,20 +86,21 @@ class Decode(Screen, GridLayout):
 
         self.decoded_msg = decode_image(Image.open(image_directory))
         print(self.decoded_msg)
-
+    #function that opens up the popup
     def firePopup(self):
         PU = decPU()
         PU.open()
         PU.ids.decodedLabel.text = self.decoded_msg.strip()
 
+    # function to check whether the user has inputted a valid directory, so the image can be displayed
     def update(self):
         self.ids.img2.source = self.ids.dir2.text
 
-
+#decode popup object
 class decPU(Popup):
     pass
 
-
+#received image popup object
 class recPU(Popup):
     receive = None
     pu = None
@@ -128,10 +131,11 @@ class recPU(Popup):
         print(recPU.pu.image_dir)
         recPU.pu.update()
 
-
+#translates the .kv file and links it to the .py file
 presentation = Builder.load_file("GUI.kv")
 
 
+#runs the GUI as an application
 class FirstApp(App):
     def build(self):
         return presentation
