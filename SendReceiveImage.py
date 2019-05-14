6eb8cb5fdd03ea4f220b2d8f2f3a4df1c33fb610
eshels <eshel.sinclair@gmail.com>
2019-05-14 14:34:08 +0300

    added while loop

--- SendReceiveImage.py
+++ SendReceiveImage.py
@@ -31,30 +31,31 @@ def recv_image(accept_image=lambda: False, directory_callback=lambda: False, dir
     server_socket.bind(('0.0.0.0', 5990))
     server_socket.listen(1)
     try:
-        (client_socket, client_address) = server_socket.accept()
-        print('connected to {}'.format(client_address))
+        while True:
+            (client_socket, client_address) = server_socket.accept()
+            print('connected to {}'.format(client_address))
 
-        command = client_socket.recv(1024)
-        print(command == CommunicationConstants.commands['INCOMING_IMAGE'])
+            command = client_socket.recv(1024)
+            print(command == CommunicationConstants.commands['INCOMING_IMAGE'])
 
-        if command == CommunicationConstants.commands['INCOMING_IMAGE'] and accept_image():
-            print('recving img')
-            image_data = b''
-            data = b''
-            while data != CommunicationConstants.commands['END_TX']:
-                # print(data)
-                data = client_socket.recv(1024)
-                if len(data) < 1024:
-                    data = data[:-2]
-                    # print(data[-2:])
+            if command == CommunicationConstants.commands['INCOMING_IMAGE'] and accept_image():
+                print('recving img')
+                image_data = b''
+                data = b''
+                while data != CommunicationConstants.commands['END_TX']:
+                    # print(data)
+                    data = client_socket.recv(1024)
+                    if len(data) < 1024:
+                        data = data[:-2]
+                        # print(data[-2:])
+                        image_data += data
+                        break
                     image_data += data
-                    break
-                image_data += data
 
-            image_dir = '{}/recv {}.png'.format(directory, datetime.now())
-            with open(image_dir, 'wb') as f:
-                f.write(image_data)
-            directory_callback(image_dir)
+                image_dir = '{}/recv {}.png'.format(directory, datetime.now())
+                with open(image_dir, 'wb') as f:
+                    f.write(image_data)
+                directory_callback(image_dir)
     finally:
         server_socket.close()
         client_socket.close()
